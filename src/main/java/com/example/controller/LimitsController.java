b1ef64283a33950fe2cf1ccc46f97335daacc011
laxman7045@gmail.com <laxmanprajapati713@gmail.com>
2025-05-24 18:01:00 +0530

    updated

---
+++ src/main/java/com/example/controller/LimitsController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.configuration.Configuration;
+import com.example.entity.Limits;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits getLimits() {
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+	//	return new Limits(1, 1000);
+	}
+	
+
+}
